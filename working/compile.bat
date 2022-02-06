f72233d0c8946ec5069fecc155696a0b283217cb
Joseph <nejoseph@uchc.net>
2022-02-06 02:50:23 -0500

    added everything except paks

--- working/compile.bat
+++ working/compile.bat
@@ -5,25 +5,25 @@ cd C:\QuakeDev\working
 
 
 echo Copying Files...
-copy C:\QuakeDev\working\terrain.map C:\QuakeDev\working
+copy C:\QuakeDev\working\shamjam.map C:\QuakeDev\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-C:\QuakeDev\tools\ericw-tools-v0.18.1-32-g6660c5f-win32\bin\qbsp.exe terrain
+C:\QuakeDev\tools\ericw-tools-v0.18.1-32-g6660c5f-win32\bin\qbsp.exe shamjam
 
 echo --------------VIS---------------
-C:\QuakeDev\tools\ericw-tools-v0.18.1-32-g6660c5f-win32\bin\vis.exe terrain
+C:\QuakeDev\tools\ericw-tools-v0.18.1-32-g6660c5f-win32\bin\vis.exe shamjam
 
 echo -------------LIGHT--------------
-C:\QuakeDev\tools\ericw-tools-v0.18.1-32-g6660c5f-win32\bin\light.exe -soft -extra4 terrain
+C:\QuakeDev\tools\ericw-tools-v0.18.1-32-g6660c5f-win32\bin\light.exe -soft -extra4 shamjam
 
-copy terrain.bsp C:\QuakeDev\id1\maps
-copy terrain.pts C:\QuakeDev\id1\maps
-copy terrain.lit C:\QuakeDev\id1\maps
+copy shamjam.bsp C:\QuakeDev\id1\maps
+copy shamjam.pts C:\QuakeDev\id1\maps
+copy shamjam.lit C:\QuakeDev\id1\maps
 pause
 cd\
 cd C:\QuakeDev
-quakespasm  +map terrain
+quakespasm  +map shamjam
--- working/run.bat
+++ working/run.bat
@@ -1,3 +1,3 @@
 cd\
 cd C:\QuakeDev
-quakespasm  +map terrain
+quakespasm  +map shamjam
